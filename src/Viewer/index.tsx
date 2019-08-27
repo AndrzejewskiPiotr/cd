7e291661286b397231318e954a0aec00956a2eee
pandrzejewski <pandrzejewski@task.gda.pl>
2019-08-27 10:40:55 +0200

    sdsdsd

--- src/Viewer/index.tsx
+++ src/Viewer/index.tsx
@@ -1,26 +1,29 @@
-import React  from 'react';
+import React, { useRef } from 'react';
 
+import ImageViewer from './viewer';
 import { usePromise } from "../hook";
 import fetchImageData from "../api/api";
 import './styles.css'
 
 function SlideWorkBench({ url }: { url:string }) {
-    const [data, isError] = usePromise(fetchImageData(`${url}/info.json`), []);
+  const container = useRef<HTMLInputElement>(null);
+  const [data, isError] = usePromise(fetchImageData(`${url}/info.json`), []);
 
-    const isDataReceived = data.length > 0;
+  const isDataReceived = data.length > 0;
+  const { current: isContainerMounted } = container;
 
+  if( isDataReceived && isContainerMounted ) {
+    ImageViewer(data,url)
+  }
 
-    return (
-        <React.Fragment>
-            { isError && !isDataReceived ?
-                null :
-                   <div>
-                     dsd, {console.log(data,isError)}
-                   </div>
-            }
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      { isError && !isDataReceived ?
+        null :
+        <div id='map-container' ref={container}/>
+      }
+    </React.Fragment>
+  )
 };
 
 export default SlideWorkBench
-
