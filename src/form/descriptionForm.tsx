3d35a17f26fa8eff5d85ba0a131abd4f3ceb3586
pandrzejewski <pandrzejewski@task.gda.pl>
2019-11-05 11:18:28 +0100

    sdsdf

--- src/form/descriptionForm.tsx
+++ src/form/descriptionForm.tsx
@@ -1,11 +1,77 @@
-import React from 'react';
-import Wrapper from './descriptionForm-styled';
-import { usePromise } from '../hook';
-import fetchDescription from '../api/description';
+import React from 'react'
+import {
+    Formik,
+    FormikProps,
+    FormikValues } from 'formik';
+import * as yup from 'yup'
 
-function DescriptionForm() {
-  const [description] = usePromise(fetchDescription(), []);
-  return <Wrapper>{`${description[0]}`}</Wrapper>;
+
+import CommonField from './common/Field/field'
+import { usePromise } from "../hook";
+import fetchDescription from "../api/description";
+import  Wrapper  from './descriptionForm-styled';
+
+export interface FormValues {
+    classification: object
 }
 
-export default DescriptionForm;
+const initialValues: FormValues = {
+    classification: {
+        code: ''
+    }
+};
+
+
+const FormSchema = yup.object().shape({
+    classification: yup.object().shape({
+        code: yup.string().required('Pole jest wymagane !'),
+    })
+});
+
+
+function DescriptionForm() {
+    const [description, isError,isPending] = usePromise(fetchDescription(),[]);
+    let value: FormValues;
+    if (description.length !== 0) {
+        let [id,standard,code] = description;
+        value = {
+            classification: {
+                code: code
+            }
+        }
+    }
+    else {
+        value = initialValues
+    }
+
+    return (
+        <Wrapper>
+            <Formik
+                initialValues={value}
+                enableReinitialize={true}
+                onSubmit={(
+                    values: FormikValues,
+                    actions: FormikValues) => {
+                        alert(JSON.stringify(values, null, 2));
+                        actions.setSubmitting(false);
+                }}
+                validationSchema={FormSchema}
+                render={(props: FormikProps<FormikValues>) => (
+                    <form onSubmit={props.handleSubmit}>
+                        <CommonField
+                            inputName="classification.code"
+                            type="text"
+                            placeholder="Uzupełnij numer choroby"
+                            htmlFor="code"
+                            label="Numer choroby"
+                            {...props}
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+            />
+        </Wrapper>
+    )
+}
+
+export default DescriptionForm
