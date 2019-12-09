6415ac9f4c8ffc7847c8365a1c71655f513fbcc2
pandrzejewski <pandrzejewski@task.gda.pl>
2019-12-09 10:59:09 +0100

    asdaasd

--- src/Viewer/viewer.tsx
+++ src/Viewer/viewer.tsx
@@ -7,10 +7,7 @@ import 'leaflet-draw/dist/leaflet.draw.css'
 
 function ImageViewer(data: number[], url: string): void {
   const [width, height, tileWidth, tileHeight, depth] = data;
-  const container: any = L.DomUtil.get('map-container');
-  if(container != null){
-    container._leaflet_id = null;
-  }
+
   const mp = L.map('map-container', {
     crs: L.CRS.Simple,
     zoomControl:false,
