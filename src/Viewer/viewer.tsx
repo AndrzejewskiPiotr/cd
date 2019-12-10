15e7f8fb5225ffe848866333b4fc06996cd0fd8e
pandrzejewski <pandrzejewski@task.gda.pl>
2019-12-10 11:53:42 +0100

    sdsdfdsf

--- src/Viewer/viewer.tsx
+++ src/Viewer/viewer.tsx
@@ -8,6 +8,7 @@ import 'leaflet-draw';
 import 'leaflet-draw/dist/leaflet.draw.css';
 
 function ImageViewer(data: number[], url: string): void {
+  const editableLayers = new L.FeatureGroup();
   const [width, height, tileWidth, tileHeight, depth] = data;
   const container: any = L.DomUtil.get('map-container');
   if (container != null) {
@@ -26,24 +27,17 @@ function ImageViewer(data: number[], url: string): void {
   );
   mp.setView([-height / 2, width / 2], -7);
 
-  L.control
-    .zoom({
-      position: 'topright'
-    })
-    .addTo(mp);
+  mp.addLayer(editableLayers);
   // @ts-ignore
-  mp.addControl(
-    L.control
-      // @ts-ignore
-      .fullscreen({
-        position: 'topright',
-        title: 'Show me the fullscreen !',
-        titleCancel: 'Exit fullscreen mode',
-        content: null,
-        forcePseudoFullscreen: false
-      })
-      .addTo(mp)
-  );
+  L.control.zoom({ position: 'topright' }).addTo(mp);
+  // @ts-ignore
+  mp.addControl(L.control.fullscreen({
+    position: 'topright',
+    title: 'Show me the fullscreen !',
+    titleCancel: 'Exit fullscreen mode',
+    content: null,
+    forcePseudoFullscreen: false,
+  }).addTo(mp));
 }
 
 export default ImageViewer;
